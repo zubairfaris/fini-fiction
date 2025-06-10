48d0e428ff7e59f794e08fa71f2142e0b0f91009
Zubair Faris <47226645+zubairfaris@users.noreply.github.com>
2025-06-10 18:51:58 -0500

    add app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,9 @@
+### In terminal, install python environment
+# python -m venv fini_fiction
+# .\fini_fiction\Scripts\activate  
+### Create requirements.txt in the same folder and include all libraries
+# pip install -r requirements.txt
+
+import streamlit as st 
+
+st.write('Hello World!')
