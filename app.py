3a28ecd65802d28752a835c08dd603ccfc618c91
Zubair Faris <47226645+zubairfaris@users.noreply.github.com>
2025-06-10 19:33:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,19 @@
 
 import streamlit as st 
 
-st.write('Hello World!')
+st.title('Welcome to Fini Fiction!')
+
+input, output= st.tabs(['Input', 'Output'])
+
+with input:
+    a = st.text_input('Insert book title', value='A lit­tle life: a nov­el')
+    b = st.text_input('Insert book settings', value='town, place, country or a year')
+    c = st.text_area('The book opens with')
+    d = st.text_area('The story is about')
+    e = st.text_area('I enjoyed the')
+    
+    st.subheader('Please rate this book:')
+    sentiment_mapping = ["one", "two", "three", "four", "five"]
+    f = st.feedback(options='stars')
+    if f is not None:
+        st.markdown(f"You selected {sentiment_mapping[f]} star(s).")
